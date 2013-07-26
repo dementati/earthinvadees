6a8fcde9752d3a0446e4f5b3ffc5888cbe9a12dc
Skraemkorp <ztargaser@gmail.com>
2013-07-26 21:06:45 +0200

    Simple AI ship controller.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from gameobjects.vector2 import Vector2
 from gameobjects.vector3 import Vector3
 from gameobjects.util import pi
+import math
 
 import util
 
@@ -30,10 +31,10 @@ class Ship:
 		self.velocity -= self.speed
 
 	def turn_left(self):
-		self.direction = util.rotate_vector2(self.direction, self.turn_speed)
+		self.direction = util.rotate_v(self.direction, self.turn_speed)
 
 	def turn_right(self):
-		self.direction = util.rotate_vector2(self.direction, -self.turn_speed)
+		self.direction = util.rotate_v(self.direction, -self.turn_speed)
 
 	def update(self):
 		self.position += self.direction*self.velocity
@@ -43,7 +44,42 @@ class Ship:
 		p2 = self.position + self.direction*self.length/2
 		pygame.draw.line(surface, self.color, p1.as_tuple(), p2.as_tuple(), 3)  
 
+class AIShipController:
+	def __init__(self, ship, accThreshold):
+		self.ship = ship
+		self.accThreshold = accThreshold
+
+	def set_target(self, target):
+		self.target = target
+
+	def update(self):
+		# Change direction
+		s2t = self.target.position - self.ship.position
+		a = util.angle_between_v(self.ship.direction, s2t)
+		if a < 0:
+			self.ship.turn_right()
+		elif a > 0:
+			self.ship.turn_left()
+
+		# Change speed
+		if math.fabs(a) < 10:
+			self.ship.accelerate()	
+		elif math.fabs(a) > 20:
+			self.ship.decelerate()
+
+class DummyTarget:
+	def __init__(self, position):
+		self.position = position
+
+	def render(self, surface):
+		px = int(self.position.x)
+		py = int(self.position.y)
+		pygame.draw.circle(surface, (0, 255, 0), (px, py), 10, 2)
+
 ship = Ship(Vector2(100, 100), Vector2(0, 1), 10, Vector3(255, 0, 0), 0.1, 2*pi/180)
+target = DummyTarget(Vector2(500, 300))
+controller = AIShipController(ship, 100)
+controller.set_target(target)
 
 while True:
 
@@ -65,10 +101,12 @@ while True:
 	if keystate[K_d]:
 		ship.turn_right()
 
+	controller.update()
 	ship.update()
 
 	# Render
 	screen.blit(background, (0, 0))
 	ship.render(screen)
+	target.render(screen)
 	pygame.display.flip()
 
--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import math
 
 from gameobjects.vector2 import Vector2
 
-def rotate_vector2(v, angle):
+def rotate_v(v, angle):
 	cs = math.cos(angle)
 	sn = math.sin(angle)
 
@@ -10,3 +10,9 @@ def rotate_vector2(v, angle):
 	py = v.x*sn + v.y*cs
 
 	return Vector2(px, py)
+
+def dot_v(v1, v2):
+	return v1.x*v2.x + v1.y*v2.y
+
+def angle_between_v(v1, v2):
+	return math.atan2(v1.x*v2.y - v1.y*v2.x, dot_v(v1,v2)) * 180.0/math.pi
